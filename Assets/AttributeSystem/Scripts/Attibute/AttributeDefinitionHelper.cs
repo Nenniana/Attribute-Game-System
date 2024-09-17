408c53140c62e04ab066c4775f023755e0f871d9
Nenniana <Tsbchrist@gmail.com>
2024-09-18 00:47:28 +0200

    Remove unused functions

--- Assets/AttributeSystem/Scripts/Attibute/AttributeDefinitionHelper.cs
+++ Assets/AttributeSystem/Scripts/Attibute/AttributeDefinitionHelper.cs
@@ -5,7 +5,6 @@ using Sirenix.Utilities.Editor;
 
 namespace AttributeSystem {
     public static class AttributeDefinitionHelper {
-        private static Dictionary<string, BaseAttributeDefinition> baseAttributeDefinitionsByName;
         private static Dictionary<string, EnhanceAttributeDefinition> enhanceAttributeDefinitionsByName;
 
         private static void BuildEnhanceAttributeDefinitionCache()
@@ -33,33 +32,6 @@ namespace AttributeSystem {
             BuildEnhanceAttributeDefinitionCache();
 
             return enhanceAttributeDefinitionsByName.TryGetValue(name, out var attributeDefinition) ? attributeDefinition : null;
-        }
-
-        private static void BuildBaseAttributeDefinitionCache()
-        {
-            if (baseAttributeDefinitionsByName != null)
-                return;
-
-            List<BaseAttributeDefinition> allAttributeDefinitions = AssetUtilities.GetAllAssetsOfType<BaseAttributeDefinition>().ToList();
-            baseAttributeDefinitionsByName = new Dictionary<string, BaseAttributeDefinition>();
-
-            foreach (var attributeDefinition in allAttributeDefinitions)
-            {
-                if (attributeDefinition != null)
-                {
-                    if (!baseAttributeDefinitionsByName.ContainsKey(attributeDefinition.name))
-                    {
-                        baseAttributeDefinitionsByName[attributeDefinition.name] = attributeDefinition;
-                    }
-                }
-            }
-        }
-
-        public static BaseAttributeDefinition GetBaseAttributeDefinitionByName(string name)
-        {
-            BuildBaseAttributeDefinitionCache();
-
-            return baseAttributeDefinitionsByName.TryGetValue(name, out var attributeDefinition) ? attributeDefinition : null;
         }
     }
 }
